2cf0eb4abdbb8e2afe0eef8ed530b4383a53fef9
rachestanislao <rachestanislao27@gmail.com>
2024-05-18 23:00:48 +0800

    Add files via upload

---
+++ Estanislao - Activity 5 (Posterior).py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 19:55:01 2024
+
+@author: rjestanislao
+"""
+import scipy.stats as sts
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu = np.linspace(1.65, 1.8, num=50)
+test = np.linspace(0, 2)
+uniform_dist = sts.uniform.pdf(mu) + 1
+uniform_dist = uniform_dist / uniform_dist.sum()
+beta_dist = sts.beta.pdf(mu, 2, 5, loc=1.65, scale=0.2)
+beta_dist = beta_dist / beta_dist.sum()
+
+
+def likelihood_func(datum, mu):
+    likelihood_out = sts.norm.pdf(datum, mu, scale=0.1)
+    return likelihood_out / likelihood_out.sum()
+likelihood_out = likelihood_func(1.7, mu)
+
+
+import scipy as sp
+
+unnormalized_posterior = likelihood_out * uniform_dist
+plt.plot(mu, unnormalized_posterior)
+plt.xlabel("$\mu$ in meters")
+plt.ylabel("Unnormalized Posterior")
+plt.show()
